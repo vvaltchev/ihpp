5e21f61c9473d03a10a4b5bbedccb74a28b4f2f2
darthvader88 <vladi32@gmail.com>
2012-08-28 23:38:13 +0200

    thprog6.c created

---
+++ tests/thprog6.c
@@ -0,0 +1,15 @@
+void bar(int n); void f3() { }
+
+void foo() { 
+
+	bar(0); 
+	f3(); 
+	bar(1); 
+}
+
+void bar(int n) { 
+
+	if (n) 
+		f3(); 
+}
+int main(int argc, char ** argv) { foo(); return 0; }
