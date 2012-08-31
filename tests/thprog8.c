1a15993c2d2c414e394a5421a6f4ca917dcff77b
Vladislav K. Valtchev <vladi32@gmail.com>
2012-08-31 17:03:43 +0200

    wot

--- tests/thprog8.c
+++ tests/thprog8.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
 #include <setjmp.h>
+#include <stdarg.h>
 
 jmp_buf jump_buffer;
 
 void testJmp(int par, ...) {
 
+	va_list ap;
+
 	if (par) 
 		longjmp(jump_buffer, 1);
 
 	printf("par is zero!\n");
+
+	va_start(ap, par);
+
+	while (par != -1) {
+
+		par = va_arg(ap, int);
+		printf("arg: %i\n", par);
+	}
+	va_end(ap);
 }
 
 int main(int argc, char ** argv) {
@@ -20,7 +32,7 @@ int main(int argc, char ** argv) {
 	
 	} else {
 
-		testJmp(0, 25, 100, 386, 4096);
+		testJmp(0, 25, 100, 386, -1);
 	}	
 
 	return 0;
